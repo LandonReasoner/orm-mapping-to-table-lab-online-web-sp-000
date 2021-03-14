06083e172ce3aec892556b642f266b1e23f41e74
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-14 17:29:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id 
   
-  d
+  def initialize(name, grade, id = nil)
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
